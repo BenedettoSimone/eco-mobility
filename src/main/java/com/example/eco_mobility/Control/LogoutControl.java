2733f734718d4c1ca5ecb883637357f682ddf1df
roccopagliarulo <72402330+roccopagliarulo@users.noreply.github.com>
2021-06-23 15:20:28 +0200

    Logout

---
+++ src/main/java/com/example/eco_mobility/Control/LogoutControl.java
@@ -0,0 +1,27 @@
+package com.example.eco_mobility.Control;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "LogoutControl", value = "/LogoutControl")
+public class LogoutControl extends HttpServlet {
+
+    public LogoutControl() {
+        super();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getSession().invalidate();
+
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("src/main/webapp/login.jsp");
+        dispatcher.forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
