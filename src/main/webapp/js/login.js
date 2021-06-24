0aa32add33bdf703b00ce282e81db7a637180dc0
benedettosimone <sim.benede@gmail.com>
2021-06-24 22:57:20 +0200

    add regex for registration

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -47,3 +47,124 @@ function selectLogin(){
 
 }
 
+
+var emailGlobal = false;
+
+
+function emailObserver() {
+
+    var email = $('#email');
+    var label = $('#email_l');
+
+    var email_text = email.val()
+
+    var reg = new RegExp(/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/);
+
+    if (!reg.test(email_text) || email_text.length < 10 || email_text.length > 40) {
+        email.css('border-color', 'red');
+        label.css('color', 'red');
+        label.text("Inserire email corretta!");
+
+        emailGlobal = false;
+
+    } else {
+        label.text("Email");
+
+
+        emailGlobal=  true;
+    }
+}
+
+function nomeObserver() {
+
+    var nome = $('#nome');
+    var label = $('#nome_l');
+
+    var nome_text = nome.val();
+
+    var reg = new RegExp(/^[a-zA-Z ]{1,20}$/);
+
+    if (!reg.test(nome_text)) {
+        nome.css('border-color', 'red');
+        label.css('color', 'red');
+        label.text("Nome non valido!");
+
+        return false;
+
+    } else {
+        label.text("Nome");
+
+        return true;
+    }
+}
+
+
+function cognomeObserver() {
+
+    var cognome = $('#cognome');
+    var label = $('#cognome_l');
+
+    var cognome_text = cognome.val();
+
+    var reg = new RegExp(/^[a-zA-Z ]{1,20}$/);
+
+    if (!reg.test(cognome_text)) {
+        cognome.css('border-color', 'red');
+        label.css('color', 'red');
+        label.text("Cognome non valido!");
+
+        return false;
+    } else {
+        label.text("Cognome");
+
+
+        return true;
+    }
+
+}
+
+function passwordObserver() {
+
+    var password = $('#password');
+    var label = $('#password_l');
+    var password_text = password.val();
+
+    var reg = new RegExp(/^[a-zA-Z0-9!@#%*)(+=._-]{8,20}$/g);
+
+    if (!reg.test(password_text)) {
+        password.css('border-color', 'red');
+        label.css('color', 'red');
+        label.text("Password non valida, mimino 8 caratteri!");
+
+
+        return false;
+
+    } else {
+        label.text("Password");
+
+        return true;
+
+    }
+
+}
+
+
+$(document).on("submit", "#fRegi", function (event) {
+
+    emailObserver();
+    let nome = nomeObserver();
+    let cognome = cognomeObserver();
+    let password = passwordObserver();
+
+    if (emailGlobal && nome && cognome && password){
+
+        return true;
+    } else {
+        return false;
+    }
+});
+
+
+
+
+
