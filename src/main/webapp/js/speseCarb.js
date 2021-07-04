3764040a343b9163cd08e7a4b7c39fc9feda2a7e
benedettosimone <sim.benede@gmail.com>
2021-07-04 22:26:29 +0200

    add regex speseCarb

---
+++ src/main/webapp/js/speseCarb.js
@@ -0,0 +1,35 @@
+function euroObserver() {
+
+    var euro = $('#euro_input');
+    var euro_l= $('#euro_label')
+    var euro_text = euro.val();
+
+    var reg = new RegExp(/^[0-9]*$/);
+
+    if (!reg.test(euro_text)) {
+        euro.css('border-color', 'red');
+        euro_l.text("Inserisci un valore numerico!");
+        euro_l.css('color', 'red');
+
+        return false;
+    } else {
+        euro.css('border-color', '#707070');
+        euro_l.text("");
+
+
+        return true;
+    }
+
+}
+
+$(document).on("submit", "#fSpesa", function (event) {
+    let euroObs = euroObserver();
+
+    if (euroObs){
+
+        return true;
+    } else {
+        return false;
+    }
+});
+
