0c7159e3d4ff239e5574291a57fbec1709fc6947
benedettosimone <sim.benede@gmail.com>
2021-07-06 20:24:48 +0200

    add regex form spostamenti

---
+++ src/main/webapp/js/spostamenti.js
@@ -0,0 +1,35 @@
+function chilometriObserver() {
+
+    var chilometri = $('#chilometri_input');
+    var chilometri_l= $('#chilometri_label')
+    var chilometri_text = chilometri.val();
+
+    var reg = new RegExp(/^[0-9]*$/);
+
+    if (!reg.test(chilometri_text)) {
+        chilometri.css('border-color', 'red');
+        chilometri_l.text("Inserisci un valore numerico!");
+        chilometri_l.css('color', 'red');
+
+        return false;
+    } else {
+        chilometri.css('border-color', '#707070');
+        chilometri_l.text("");
+
+
+        return true;
+    }
+
+}
+
+$(document).on("submit", "#fMezzo", function (event) {
+    let kmObs = chilometriObserver();
+
+    if (kmObs){
+
+        return true;
+    } else {
+        return false;
+    }
+});
+
