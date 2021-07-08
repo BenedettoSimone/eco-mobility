b293c3a07e507f76ff58ae672c6eae5ff872c193
benedettosimone <sim.benede@gmail.com>
2021-07-08 21:45:39 +0200

    add cookie

---
+++ src/main/webapp/js/cookie.js
@@ -0,0 +1,24 @@
+/*vedo se ci vuole la dark mode*/
+window.onload = function provaDark(){
+    var body = document.getElementById("body");
+    let name = "darkmode=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            alert(c.substring(name.length, c.length)+" cookie recuperato");
+
+            body.className="dark-mode";
+
+            return;
+        }
+    }
+
+    alert("cookie non recuperato");
+    body.className="light-mode";
+
+}
